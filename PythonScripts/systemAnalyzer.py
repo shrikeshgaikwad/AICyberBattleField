52f6ad4547267f9c22673ddc62ab7a7bd057b9b3
Shrikesh Gaikwad <shrikeshgaikwad@gmail.com>
2026-01-27 01:25:48 +0530

    8

--- PythonScripts/systemAnalyzer.py
+++ PythonScripts/systemAnalyzer.py
@@ -11,18 +11,21 @@ print(f"Scannig started for {target}")
 
 
 scanner.scan(target,ports)
-for host in scanner.all_hosts():
-    print("Host:", host)
-    print("State:", scanner[host].state())
 
-    for proto in scanner[host].all_protocols():
-        print("Protocol:", proto)
+if target in scanner.all_hosts():
+    for host in scanner.all_hosts():
+        print("Host:", host)
+        print("State:", scanner[host].state())
 
-        ports = scanner[host][proto].keys()
-        for port in ports:
-            state = scanner[host][proto][port]['state']
-            print(f"Port {port}: {state}")
+        for proto in scanner[host].all_protocols():
+            print("Protocol:", proto)
 
+            ports = scanner[host][proto].keys()
+            for port in ports:
+                state = scanner[host][proto][port]['state']
+                print(f"Port {port}: {state}")
+else:
+    print("host down or unable to connect ")
 print("done")
 
 
